f58cda2f4e3475034bf676cf37d1207704d4cdde
Adonias <adoniashaile1@gmail.com>
2023-12-23 09:17:42 +0300

    views-urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", views.monthly_challenges),
+    path("",views.index),
+    path("<int:month>", views.challengesByNumber),
+    path("<str:month>", views.monthly_challenges, name="month-challenge"),
 ]
+
--- challenges/views.py
+++ challenges/views.py
@@ -1,21 +1,43 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+monthsDec = {
+    "jan": "jan",
+    "feb": "feb",
+    "mar": "mar",
+    "apr": "april",
+    "may": "may",
+}
 
 
 # Create your views here.
-def january(request):
-    return HttpResponse("working")
+def index(request):
+    listItems = ""
+    months = list(monthsDec.keys())
+    for month in months:
+        capitilized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        listItems += f'<li><a href="{month_path}">{capitilized_month}</a></li>'
+
+    response_data = f"<ul>{listItems}</ul>"
+    return HttpResponse(response_data)
+
+
+def challengesByNumber(request, month):
+    months = list(monthsDec.keys())
+
+    redirect_month = months[month - 1]
+    if month > len(months):
+        return HttpResponseNotFound("page not available")
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenges(resquest, month):
-    challenge_text = None
+    try:
+        challenge_text = monthsDec[month]
+        return HttpResponse(challenge_text)
 
-    if month == "february":
-        challenge_text = "welcome to february"
-    elif month == "jan":
-        challenge_text = "welcome to january"
-    elif month == "ju":
-        challenge_text = "welcome to ju"
-    else:
-        return HttpResponseNotFound("no page is availble for your request")
-    return HttpResponse(challenge_text)
+    except:
+        HttpResponseNotFound("page not found")
