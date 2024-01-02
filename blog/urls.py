88dc33efea94d98414480d8568f15af3ca4238c1
Adonias <adoniashaile1@gmail.com>
2024-01-02 20:51:26 +0300

    django blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting_page"),
     path("posts/", views.posts, name="posts_page"),
-    path("posts/<slug>", views.post_detail, name="post_page_detail"),
+    path("posts/<slug:slug>", views.post_detail, name="post_page_detail"),
 ]
