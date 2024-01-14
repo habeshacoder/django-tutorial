c8a63dae267deac06aa1d6455e5ef286320815e5
Adonias <adoniashaile1@gmail.com>
2024-01-14 10:16:39 +0300

    models in django

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from blog.models import Post, Author, Tag
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','date','author')
+    prepopulated_fields={"slug":('title',)}
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','email')
 # Register your models here.
+admin.site.register(Post,PostAdmin)
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Tag)
